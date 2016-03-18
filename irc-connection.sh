d18d766a187427daa9e1c77394c78a2a00a9284c
st2092 <sto002@ucr.edu>
2016-03-18 00:29:26 -0700

    Add inputs verification

--- irc-connection.sh
+++ irc-connection.sh
@@ -5,6 +5,27 @@
 # to exchange messages required for initialization. Once connection gets 
 # establish we maintain connection by replying to PING messages from the server.
 
+# function: verifyInputs
+# This function checks the inputs from the user to make sure it is valid.
+# Valid inputs has no whitespaces.
+function verifyInputs() 
+{
+    if [[ -n "$nn_extra1$nn_extra2" ]] || [[ -n "$un_extra1$un_extra2" ]]
+    then
+        # one or more input is invalid
+        #printf >&2 "%s: %s\n" "$0" "$1"
+        printf "Nick name and username must not contain any spaces."
+        exit "${2:-1}"
+    fi
+}
+
+# function: printInputs
+# This method prints out the user inputs for server, port, nick name, username, and real name.
+function printInputs()
+{
+    printf "Server: $server,\nPort: $port,\nNick name: $nick_name,\nUsername: $username,\nReal name: $real_name\n"
+}
+
 printf "Server: "
 read server
 
@@ -12,12 +33,12 @@ printf "Port: "
 read port
 
 printf "Nick name: "
-read nick_name
+read nick_name nn_extra1 nn_extra2    # read assume space character as delimiter; any value set for extra# indicate spaces in user input
 
 printf "Username: "
-read username
+read username un_extra1 un_extra2
 
 printf "Real name: "
 read real_name
 
-printf "Server: $server,\nPort: $port,\nNick name: $nick_name,\nUsername: $username,\nReal name: $real_name\n"
+verifyInputs
